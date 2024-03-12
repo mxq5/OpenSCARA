29d3907f20cdf3e3bb73f33f8e3091f13871f074
mxq <bartoloomew@gmail.com>
2024-03-12 16:44:06 +0100

    Arm class done, working on IK

--- Software/RoboSyncDesktop/scripts/3DView.js
+++ Software/RoboSyncDesktop/scripts/3DView.js
@@ -8,13 +8,13 @@ class Arm {
             Z: {
                 model: null,
                 offsets: { x: 0, y: 0.8, z: 0 },
-                angle: 0,
                 height: 0,
             },
             J1: {
                 model: null,
                 offsets: { x: 0, y: 1.1, z: 0 },
                 angle: 0,
+                armlength: 2.9
             },
             J2: {
                 model: null,
@@ -23,7 +23,7 @@ class Arm {
             },
             gripper: {
                 model: null,
-                offsets: { x: 0, y: 0.7, z: 3.4 },
+                offsets: { x: 0, y: 0.7, z: 6 },
                 angle: 0,
             },
         };
@@ -34,16 +34,18 @@ class Arm {
             "J1-axis",
             "J2-axis",
         ];
-        
-        this.Zangle = 0;
-        
-        this.J1angle = 0;
-        this.J2angle = 0;
 
         this.render = render;
         this.loader = new OBJLoader();
         this.scene = scene;
         this.setupAxes();
+
+        this.IKIndicator = { 
+            x: this.joints.gripper.offsets.x - 1, 
+            y: 0 + 2, 
+            z: this.joints.gripper.offsets.z - 1.2
+        };
+        this.IKindicatorModel = this.spawnIKIndicator();
     }
 
     loadModel(filename) {
@@ -98,29 +100,90 @@ class Arm {
     }
 
     setJ1Angle(angle) {
-        this.Zangle = angle
-        this.J1angle = angle;
+        this.joints.J1.angle = angle;
     
-        this.joints.Z.model.rotation.set(0, this.Zangle, 0);
-        this.joints.J1.model.rotation.set(0, this.J1angle, 0);
+        this.joints.Z.model.rotation.set(0, this.joints.J1.angle, 0);
+        this.joints.J1.model.rotation.set(0, this.joints.J1.angle, 0);
     
         // Position correction for joint J2
-        let x = Math.sin(this.J1angle) * 2.9;
-        let z = Math.cos(this.J1angle) * 2.9;
-        
-        this.joints.J2.model.rotation.set(0, (this.J1angle + this.J2angle), 0);
-        
-        // TODO: extract constant to a variable
-        this.joints.J2.model.position.set(x, 2.6, z);
+        this.joints.J2.offsets.x = Math.sin(this.joints.J1.angle) * this.joints.J1.armlength;
+        this.joints.J2.offsets.z = Math.cos(this.joints.J1.angle) * this.joints.J1.armlength;
+
+        this.joints.J2.model.rotation.set(0, (this.joints.J1.angle + this.joints.J2.angle), 0);
+        this.joints.J2.model.position.set(
+            this.joints.J2.offsets.x,
+            (this.joints.J2.offsets.y + this.joints.Z.height), 
+            this.joints.J2.offsets.z
+            );
 
         this.render();
     }
 
     setJ2Angle(angle) {
-        this.J2angle = angle;
-        this.joints.J2.model.rotation.set(0, (this.J1angle + this.J2angle), 0);
+        this.joints.J2.angle = angle;
+        this.joints.J2.model.rotation.set(0, (this.joints.J1.angle + this.joints.J2.angle), 0);
         this.render();
     }
+
+    setHeight(height) {
+        this.joints.Z.height = height;
+        this.joints.J1.model.position.set(
+            this.joints.J1.offsets.x, 
+            (this.joints.J1.offsets.y + this.joints.Z.height), 
+            this.joints.J1.offsets.z
+        );
+
+        this.joints.J2.model.position.set(
+            this.joints.J2.offsets.x, 
+            (this.joints.J2.offsets.y + this.joints.Z.height), 
+            this.joints.J2.offsets.z
+        );
+
+        this.render();
+    }
+
+    spawnIKIndicator() {
+        const geometry = new THREE.SphereGeometry( 0.1, 32, 32 );
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const indicatorModel = new THREE.Mesh( geometry, material );
+        
+        this.scene.add(indicatorModel);
+        indicatorModel.position.set(this.IKIndicator.x, this.IKIndicator.y, this.IKIndicator.z);
+
+        return indicatorModel;
+    }
+
+    setIKIndicatorPosition(x, y, z) {
+        this.IKindicatorModel.position.set(x, y, z);
+        this.render();
+    }
+
+    getIKIndicatorPosition() {
+        return this.IKIndicator;
+    }
+
+    inverseKinematics(x, y, z) {
+        // Odległość w lini prostej z bazy do punktu
+        let j1 = 3;
+        let j2 = 2.4;
+    
+        // Odległość od podstawy
+        let r =  x**2 + y**2;
+    
+        console.log("R", r);
+    
+        // Kąt pomiędzy ramionami
+        //let gamma = Math.acos((r  - x**2 - y**2) / (-2*x*y) );
+        //let baseAngle = Math.acos((x**2 - r - y**2) / (-2*Math.sqrt(r)*y) );
+        
+        let gamma = Math.acos(( r - j1**2 - j2**2 ) / ( -2*j1*j2 ));
+        
+        console.log("IK: " + gamma, gamma);
+    
+        const offset = 0.7;
+        return [0, gamma, z];
+    }
+
 }
 
 // Create a scene
@@ -139,7 +202,8 @@ const moveUp = document.getElementById('au');
 const moveDown = document.getElementById('ad');
 
 // Create a camera
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 1000);
+
 camera.position.set( 7, 7, 12);
 camera.lookAt( 0, 0, 0 );
 
@@ -156,151 +220,80 @@ scene.add( ambientLight );
 scene.add( light );
 
 
-// Create a red sphere
-let geometry = new THREE.SphereGeometry( 0.1, 32, 32 );
-let material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-let sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
-
 // controls
 let cameraControls = new OrbitControls( camera, renderer.domElement );
 cameraControls.addEventListener( 'change', render );
 
 // Render function
-function render() {
-    renderer.render(scene, camera);
-}
+function render() { renderer.render(scene, camera); }
 
 const arm = new Arm(scene, render);
 
 
 let j1 = 0;
 let j2 = 0
-let step = 0.3;
-
-// Start the animation loop
+const step = 0.3;
+// Controls
 moveForward.addEventListener('click', () => {
     j2++;
     arm.setJ2Angle(j2*step);
-    console.log("J1: " + j1*step + " J2: " + j2*step);
 });
 
 moveBackward.addEventListener('click', () => {
+    j2--;
+    arm.setJ2Angle(j2*step);
+});
+
+moveUp.addEventListener('click', () => {
     j1++;
     arm.setJ1Angle(j1*step);
-    console.log("J1: " + j1*step + " J2: " + j2*step);
+});
+
+moveDown.addEventListener('click', () => {
+    j1--;
+    arm.setJ1Angle(j1*step);
 });
 
 moveToHome.addEventListener('click', () => {
     console.log(arm.J1angle, arm.J2angle);
 });
 
-
-/*
-moveToHome.addEventListener('click', () => {
-    i++;
-
-    J1angle = i*step;
-    
-    joints[1].rotation.set(0, J1angle, 0);
-    joints[2].rotation.set(0, J1angle, 0);
-
-    let x = Math.sin(J1angle) * 2.9;
-    let z = Math.cos(J1angle) * 2.9;
-    joints[3].rotation.set(0, (J1angle + J2angle), 0);
-    joints[3].position.set(x, 2.6, z);
-    console.log()
-    render();
-});
-
-/*
-moveBackward.addEventListener('click', () => {
-    i--;
-
-    J1angle = i*step;
-
-    joints[1].rotation.set(0, J1angle, 0);
-    joints[2].rotation.set(0, J1angle, 0);
-
-    let x = Math.sin(J1angle) * 2.9;
-    let z = Math.cos(J1angle) * 2.9;
-    joints[3].rotation.set(0, (J1angle + J2angle), 0);
-    joints[3].position.set(x, 2.6, z);
-    console.log()
-    render();
-});
-
-moveUp.addEventListener('click', () => {
-    d++;
-    J2angle = d*step;
-    joints[3].rotation.set(0, (J1angle + J2angle), 0);
-    render();
-});
-
-moveDown.addEventListener('click', () => {
-    d--;
-    J2angle = d*step;
-    joints[3].rotation.set(0, (J1angle + J2angle), 0);
-    render();
-});
-
-// J1 length = 3
-// J2 length = 2.4
-
-let sx = 0, sy = 0, sz = 0;
+let zheight = 0;
 window.addEventListener('keydown', (event) => {
-    if(event.key === "w") {
-        sx += 0.1;
-    }
-    else if(event.key === "a") {
-        sy += 0.1;
-    }
-    else if(event.key === "s") {
-        sx -= 0.1;
-    }
-    else if(event.key === "d") {
-        sy -= 0.1;
-    }
-    else if(event.key === "q") {
-        sz -= 0.1;
+    if(event.key === "q") {
+        zheight -= 0.1;
+        arm.setHeight(zheight);
     }
     else if(event.key === "e") {
-        sz += 0.1;
+        zheight += 0.1;
+        arm.setHeight(zheight);
     }
 
-    if(event.key === "Enter") {
-        sphere.position.set(sx, sz, sy);
-        let [ baseAngle, armAngle, z] = inverseKinematics(sx, sy, sz);
-        console.log(baseAngle, armAngle, z);
-        joints[1].rotation.set(0, baseAngle, 0);
-        joints[2].rotation.set(0, baseAngle, 0);
-        joints[3].rotation.set(0, (baseAngle + armAngle), 0);
+    if(event.key === "w") {
+        arm.IKIndicator.z -= 0.1;
+        arm.setIKIndicatorPosition(arm.IKIndicator.x, arm.IKIndicator.y, arm.IKIndicator.z);
+    } else if(event.key === "s") {
+        arm.IKIndicator.z += 0.1;
+        arm.setIKIndicatorPosition(arm.IKIndicator.x, arm.IKIndicator.y, arm.IKIndicator.z);
+    } else if(event.key === "a") {
+        arm.IKIndicator.x -= 0.1;
+        arm.setIKIndicatorPosition(arm.IKIndicator.x, arm.IKIndicator.y, arm.IKIndicator.z);
+    } else if(event.key === "d") {
+        arm.IKIndicator.x += 0.1;
+        arm.setIKIndicatorPosition(arm.IKIndicator.x, arm.IKIndicator.y, arm.IKIndicator.z);
+    } else if (event.key === "c") {
+        arm.IKIndicator.y += 0.1;
+        arm.setIKIndicatorPosition(arm.IKIndicator.x, arm.IKIndicator.y, arm.IKIndicator.z);
+    } else if (event.key === "z") {
+        arm.IKIndicator.y -= 0.1;
+        arm.setIKIndicatorPosition(arm.IKIndicator.x, arm.IKIndicator.y, arm.IKIndicator.z);
     }
-
-    console.log(sx, sy, sz);
-    sphere.position.set(sx, sz, sy);
-    render();
+    
+    if (event.key === "i") {
+        const [j1, j2, z] = arm.inverseKinematics(arm.IKIndicator.x, arm.IKIndicator.y, arm.IKIndicator.z);
+        arm.setJ1Angle(j1);
+        arm.setJ2Angle(j2);
+        arm.setHeight(z);
+    }
+    
 });
-
-function inverseKinematics(x, y, z) {
-    // Zadane współrzędne
-    // sx, sz, sy
-
-    // Długości ramion
-    let j1 = 3;
-    let j2 = 2.4;
-
-    // Odległość od podstawy
-    let r =  x**2 + y**2;
-
-    console.log("R", r);
-
-    // Kąt pomiędzy ramionami
-    let gamma = Math.acos((r  - x**2 - y**2) / (-2*x*y) );
-    let baseAngle = Math.acos((x**2 - r - y**2) / (-2*Math.sqrt(r)*y) );
-
-    console.log("Gamma", gamma);
-
-    return [baseAngle, gamma, z];
-}
-*/
