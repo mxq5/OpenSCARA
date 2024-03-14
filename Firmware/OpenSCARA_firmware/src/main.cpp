6913d5143bf3d560d549ecfe39da1f9d84e81c6a
FrivKM <kubameusz12@gmail.com>
2024-03-14 02:51:56 +0100

    Basic RAMPS control added in firmware

--- Firmware/OpenSCARA_firmware/src/main.cpp
+++ Firmware/OpenSCARA_firmware/src/main.cpp
@@ -1,18 +1,105 @@
 #include <Arduino.h>
+#include <pinmap.h>
 
-// put function declarations here:
-int myFunction(int, int);
+class OpenSCARA {
+  private:
+    // Available screws: t8x8, t8x2
+    uint8_t Z_screwPitch = 2;
+
+    // Reduction ratio
+    uint8_t J1_gear_ratio = 9; // (1:3 * 1:3) gear
+    uint8_t J2_gear_ratio = 8; // (1:2 * 1:4) gear  
+    uint8_t W_gear_ratio = 4;  // (1:4) gear
+
+  public:
+    int test = 0;
+
+  OpenSCARA() {
+    // do some stuff
+  }
+};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  /*pinMode(FAN_PIN , OUTPUT);
+  pinMode(HEATER_0_PIN , OUTPUT);
+  pinMode(HEATER_1_PIN , OUTPUT);
+  pinMode(LED_PIN  , OUTPUT);
+  
+  pinMode(X_STEP_PIN  , OUTPUT);
+  pinMode(X_DIR_PIN    , OUTPUT);
+  pinMode(X_ENABLE_PIN    , OUTPUT);
+  
+  pinMode(Y_STEP_PIN  , OUTPUT);
+  pinMode(Y_DIR_PIN    , OUTPUT);
+  pinMode(Y_ENABLE_PIN    , OUTPUT);
+  
+  pinMode(Z_STEP_PIN  , OUTPUT);
+  pinMode(Z_DIR_PIN    , OUTPUT);
+  pinMode(Z_ENABLE_PIN    , OUTPUT);
+  
+  pinMode(E_STEP_PIN  , OUTPUT);
+  pinMode(E_DIR_PIN    , OUTPUT);
+  pinMode(E_ENABLE_PIN    , OUTPUT);
+  
+  pinMode(Q_STEP_PIN  , OUTPUT);
+  pinMode(Q_DIR_PIN    , OUTPUT);
+  pinMode(Q_ENABLE_PIN    , OUTPUT);
+  
+   digitalWrite(X_ENABLE_PIN    , LOW);
+   digitalWrite(Y_ENABLE_PIN    , LOW);
+   digitalWrite(Z_ENABLE_PIN    , LOW);
+   digitalWrite(E_ENABLE_PIN    , LOW);
+   digitalWrite(Q_ENABLE_PIN    , LOW);
+   */
+
+  // J1
+  pinMode(J1_STEP_PIN, OUTPUT);
+  pinMode(J1_DIR_PIN, OUTPUT);
+  pinMode(J1_ENABLE_PIN, OUTPUT);
+
+  // Enable Axis
+  digitalWrite(J1_ENABLE_PIN, LOW);
+
+  // J2
+  pinMode(J2_STEP_PIN, OUTPUT);
+  pinMode(J2_DIR_PIN, OUTPUT);
+  pinMode(J2_ENABLE_PIN, OUTPUT);
+
+  // Enable Axis
+  digitalWrite(J2_ENABLE_PIN, LOW);
+
+  // Z
+  pinMode(Z_STEP_PIN, OUTPUT);
+  pinMode(Z_DIR_PIN, OUTPUT);
+  pinMode(Z_ENABLE_PIN, OUTPUT);
+
+  // Enable Axis
+  digitalWrite(Z_ENABLE_PIN, LOW);
+
+  // W
+  pinMode(W_STEP_PIN, OUTPUT);
+  pinMode(W_DIR_PIN, OUTPUT);
+  pinMode(W_ENABLE_PIN, OUTPUT);
+
+  // Enable Axis
+  digitalWrite(W_ENABLE_PIN, LOW);
+
+  OpenSCARA scara;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void spinEveryone() {
+  digitalWrite(J1_STEP_PIN, HIGH);
+  digitalWrite(J2_STEP_PIN, HIGH);
+  digitalWrite(Z_STEP_PIN, HIGH);
+  digitalWrite(W_STEP_PIN, HIGH);
+  delay(1);
+  digitalWrite(J1_STEP_PIN, LOW);
+  digitalWrite(J2_STEP_PIN, LOW);
+  digitalWrite(Z_STEP_PIN, LOW);
+  digitalWrite(W_STEP_PIN, LOW);
+  delay(1);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop () {
+  spinEveryone();
 }
---
+++ Firmware/OpenSCARA_firmware/src/pinmap.h
@@ -0,0 +1,43 @@
+// Ramps 1.4 X - SCARA J1
+#define J1_STEP_PIN         54
+#define J1_DIR_PIN          55
+#define J1_ENABLE_PIN       38
+#define J1_MIN_PIN           3
+#define J1_MAX_PIN           2
+
+// Ramps 1.4 Y - SCARA J2
+#define J2_STEP_PIN         60
+#define J2_DIR_PIN          61
+#define J2_ENABLE_PIN       56
+#define J2_MIN_PIN          14
+#define J2_MAX_PIN          15
+
+// Ramps 1.4 Z - SCARA Z
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
+
+// Ramps 1.4 W - SCARA W (gripper rotation)
+#define W_STEP_PIN         36
+#define W_DIR_PIN          34
+#define W_ENABLE_PIN       30
+
+#define E_STEP_PIN         26
+#define E_DIR_PIN          28
+#define E_ENABLE_PIN       24
+
+#define SDPOWER            -1
+#define SDSS               53
+#define LED_PIN            13
+
+#define FAN_PIN            9
+
+#define PS_ON_PIN          12
+#define KILL_PIN           -1
+
+#define HEATER_0_PIN       10
+#define HEATER_1_PIN       8
+#define TEMP_0_PIN          13   // ANALOG NUMBERING
+#define TEMP_1_PIN          14   // ANALOG NUMBERING
