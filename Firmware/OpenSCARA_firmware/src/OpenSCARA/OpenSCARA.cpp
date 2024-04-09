1fb20d5582f411c576ab1675096c056561f68dc6
mxq <bartoloomew@gmail.com>
2024-04-09 02:47:12 +0200

    Z Axis direction tweaks

--- Firmware/OpenSCARA_firmware/src/OpenSCARA/OpenSCARA.cpp
+++ Firmware/OpenSCARA_firmware/src/OpenSCARA/OpenSCARA.cpp
@@ -121,7 +121,7 @@ void OpenSCARA::setZ(int value) {
         return;
     }
 
-    float mm_difference = Z - value;
+    float mm_difference = value;
     long steps_difference = static_cast<long>((mm_difference * (MOTOR_STEPS_PER_REVOLUTION / AXIS_Z_GEAR_RATIO)));
     long steps = AXIS_Z.currentPosition() + steps_difference;
 
